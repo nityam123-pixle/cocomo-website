2f85db38c18286aa94e38ed9988daab1253369f4
Nityam Suchak <apple@Nityams-MacBook-Air.local>
2025-05-17 13:30:21 +0530

    Done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({
 
 export const metadata: Metadata = {
     title: "Cocomo Media",
-    description: "Created with the help of Frontend Tribe",
+    description: "COCOMO MEDIA",
 };
 
 export default function RootLayout({
